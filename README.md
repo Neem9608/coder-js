edb721fef9cd80e1a0be11eb9aa8131fe4f2ffdf
admer escobar <admerescobar@gmail.com>
2023-05-14 17:19:31 -0300

    Agregando contenido para la segunda pre entrega

--- README.md
+++ README.md
@@ -1,17 +1,44 @@
-## Formulario para dar en adopcion a un canino 
+# Carrito de Compras de Cursos
 
-El código proporcionado implementa funciones, condiciones, ciclos, variables, validaciones.
+Este proyecto implementa un carrito de compras de cursos utilizando JavaScript. Permite al usuario seleccionar cursos de una lista y calcular el total a pagar.
 
-El codigo esta diseñado para validarVacio() de todas las variable que se muestren por Prompt.
-El codigo tambien esta diseñado para validar si el usuario no ingresa correctamente los caracteres que se pide para continuar.
- Si en caso el usuario no ingresa correctamente los caracteres se generará un bucle hasta que se ingrese correctamente, esta integrado el .toLowerCase() para ayudar simplificar y el usuario no tenga que ingresar  estrictamente en mayuscula o minuscula.
-El codigo muestra condiciones de edad para seguir con con el formulario.
+## Funcionalidades
 
-El codigo esta previamente comentado en cada seccion como ver como va interconetado y que hace cada linea de codigo, para luego tener un resultado final.
+- Muestra una lista de cursos disponibles para agregar al carrito.
+- Valida la selección del usuario y garantiza la entrada correcta de datos.
+- Calcula el total a pagar por los cursos seleccionados.
+- Proporciona mensajes y alertas interactivas para guiar al usuario.
 
-## Feedback
+## Uso
 
-Espero su devolucion, si hay algo ó muchas cosas que mejorar y me van a ayudar en los proximos proyectos, siempre son totalmente bienvenidos y recibidos con total aprecio.
+1. Al ejecutar el programa, se mostrará una lista de cursos disponibles.
+2. El usuario podrá indicar si desea continuar o no con la selección.
+3. En caso de continuar, se le pedirá al usuario que agregue cursos al carrito. Solo se aceptan los nombres de cursos válidos de la lista.
+4. Una vez que el usuario haya terminado de agregar cursos, se mostrará un resumen con los cursos seleccionados y el total a pagar.
+5. El usuario también podrá ver el total a pagar en la consola.
+
+## Requisitos
+
+- Navegador web compatible con JavaScript.
+
+## Contribución
+
+Si deseas contribuir a este proyecto, sigue los siguientes pasos:
+
+1. Haz un fork de este repositorio.
+2. Crea una rama con tu nueva funcionalidad: `git checkout -b nueva-funcionalidad`.
+3. Realiza los cambios y realiza los commits: `git commit -m "Agrega nueva funcionalidad"`.
+4. Sube los cambios a tu repositorio: `git push origin nueva-funcionalidad`.
+5. Crea una solicitud de extracción en este repositorio.
+
+<!-- ## Licencia
+
+Este proyecto se distribuye bajo la licencia MIT. Puedes encontrar más información en el archivo [LICENSE](LICENSE). -->
+
+## Contacto
+
+Si tienes alguna pregunta o sugerencia sobre este proyecto, no dudes en contactarme. Puedes encontrarme en [admerescobar](https://github.com/admerescobar) o enviarme un correo electrónico a [admerescobar@gmail.com](admerescobar@gmail.com).
+
 
 
 Admer Escobar (Alumno en Coderhouse - Curso de JavaScript)
--- js/main.js
+++ js/main.js
@@ -1,67 +1,113 @@
-// funcion para calcular vacio
+// Definición de la clase Curso
+class Curso {
+  constructor(id, nombre, precio) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+}
+
+const cursos = [];
+cursos.push(
+  new Curso(1, "Habilidad", 50),
+  new Curso(2, "Obediencia", 30),
+  new Curso(3, "Discapacidad", 100)
+);
+
+// Función para validar si un valor está vacío
 function validarVacio(valor) {
   let vacio = "";
-  // condicion para validar vacio
+  // Condición para validar si el valor está vacío
   if (valor == vacio) {
     return true;
   } else return false;
 }
-// declaracion de Variable nombre
-let nombre = prompt("Hola! Bienvenid@, ¿cual es tu nombre?");
-console.log("nombre del solicitante: ", nombre);
-// ciclo para validar vacio de la variable 'nombre'.
-while (validarVacio(nombre)) {
-  alert("por favor, complete este espacio");
-  nombre = prompt("cual es tu nombre?");
-  console.log("nombre del solicitante: ", nombre);
-}
-// declaracion de Variable edad
-let edad = parseInt(prompt("Bien! ¿que edad tienes?"));
-console.log("edad del solicitante: ", edad);
-// ciclo para validar vacio de la variable 'edad'.
-while (validarVacio(edad) || isNaN(edad)) {
-  edad = parseInt(prompt("ingrese una edad valida"));
-  console.log("edad del solicitante: ", edad);
-}
-// condicion para validar si la edad cumple con los requeriminetos de edad minima para adoptar al canino.
-if (edad >= 18) {
-  let telefono = parseInt(prompt("ingrese su numero de telefono"));
-  console.log("telefono del solicitante: ", telefono);
-  while (validarVacio(telefono) || isNaN(telefono)) {
-    telefono = parseInt(prompt("ingrese un telefono valido"));
-    console.log("telefono del solicitante: ", telefono);
-  }
-  // declaracion de Variable vivienda
-  let vivienda = prompt("¿usted vive en un countries? responda: 'si o no' ");
-  console.log("el solicitante vive en un coutry?: ", vivienda);
-  // ciclo para validar que el usuario ingrese el caracter correcto.
 
-  while (vivienda.toLowerCase() != "si" && vivienda.toLowerCase() != "no") {
-    alert("por Favor ingrese un caracter valido");
-    vivienda = prompt("escriba una opcion: si o no");
-  }
-  // ciclo para validar vacio de variable 'vivienda'.
-  while (validarVacio(vivienda)) {
-    alert("por favor, complete este espacio");
-    vivienda = prompt("¿usted vive en un cauntry? responda: 'si o no' ");
-    console.log("el solicitante vive en un coutry?: ", vivienda);
-  }
-  // condicion para validar si el interesado en adoptar el canino vive en un countries, el interesado tiene que elegir 'SI o NO'.
-
-  if (vivienda == "si" || vivienda == "Si" || vivienda == "SI") {
-    alert("nos estaremos contactando en breve");
-    alert("por favor, recargue la pagina con F5 para elegir otras opciones ");
-  }
-  if (vivienda == "no" || vivienda == "No" || vivienda == "NO") {
-    alert(
-      "No cumples la condicion de edad para adoptar a Diego,Estamos buscando personas que vivan en un countries. "
-    );
-    alert("por favor, recargue la pagina con F5 para elegir otras opciones ");
-  }
-  // condicion para validar si el interesado en adoptar el canino NO vive en un countries.
-} else {
-  alert(
-    "No cumples la condicion de edad para adoptar a Diego,Estamos buscando personas mayores a 17 años"
+let mostrarCursos = (arr) => {
+  message = ""; // Variable para almacenar los mensajes
+  arr.forEach(
+    (curso) => (message += `${curso.nombre} - $ ${curso.precio} USD` + "\n")
   );
-  alert("por favor, recargue la pagina con F5 para elegir otras opciones ");
+  return message;
+};
+
+let carrito = [];
+let seleccion = prompt(
+  "Bienvenid@ A continuación nuestra lista de cursos,desea continuar? \nEscriba una opcion: si o no."
+);
+console.log(seleccion);
+// Ciclo para validar si la selección está vacía
+while (validarVacio(seleccion)) {
+  alert("por favor, complete este espacio");
+  seleccion = prompt("ingrese un caracter: si o no.");
 }
+// Ciclo para validar que el usuario ingrese la opción correcta
+while (seleccion.toLowerCase() != "si" && seleccion.toLowerCase() != "no") {
+  alert("por Favor ingrese un caracter valido");
+  seleccion = prompt("escriba una opcion: si o no");
+}
+// Condición si el usuario elige "si"
+if (seleccion == "si" || seleccion == "Si" || seleccion == "SI") {
+    // Código adicional para realizar si el usuario selecciona "si"
+} else if (seleccion.toLowerCase() == "no") {
+  alert("Gracias por visitar la pagina");
+  alert("presione F5 ó Recargue la pagina  para seguir Interactuando");
+}
+while (seleccion != "no") {
+  let curso = prompt(
+    "Agrega un curso a tu carrito: \n" + mostrarCursos(cursos)
+  );
+  while (
+    curso.toLowerCase() != "habilidad" &&
+    curso.toLowerCase() != "obediencia" &&
+    curso.toLowerCase() != "discapacidad"
+  ) {
+    alert("por Favor ingrese un caracter valido");
+    curso = prompt("Agrega un curso a tu carrito: \n" + mostrarCursos(cursos));
+  }
+
+  // Ciclo para validar si el valor de "curso" está vacío
+  while (validarVacio(curso)) {
+    curso = prompt(
+      "Por favor, complete este espacio\n" + mostrarCursos(cursos)
+    );
+  }
+  let precio = 0;
+
+  // Utilizamos el método "find" para buscar el curso seleccionado en el array de cursos
+  let cursoEncontrado = cursos.find(
+    (elemento) => elemento.nombre.toLowerCase() == curso.toLowerCase()
+  );
+  carrito.push(cursoEncontrado);
+  alert(`Curso encontrado: ${cursoEncontrado.nombre}`);
+
+  precio = cursoEncontrado.precio;
+
+  seleccion = prompt("¿desea agregar otro curso? Escriba: si o no");
+  // Ciclo para validar si la selección está vacía
+  while (validarVacio(seleccion)) {
+    alert("por favor, complete este espacio");
+    seleccion = prompt("¿desea agregar otro curso? Escriba: si o no");
+  }
+  // Ciclo para validar que el usuario ingrese la opción correcta
+  while (seleccion.toLowerCase() != "si" && seleccion.toLowerCase() != "no") {
+    alert("por Favor ingrese un caracter valido");
+    seleccion = prompt("escriba una opcion: si o no");
+  }
+// Si la selección es "no", se muestra el mensaje de despedida y se imprime el resumen del carrito
+  while (seleccion == "no") {
+    alert("gracias y hasta pronto");
+    carrito.forEach((carritoFinal) => {
+      console.log(
+        `curso: ${carritoFinal.nombre}, El Total a pagar por este curso es: $${carritoFinal.precio} USD.`
+      );
+    });
+    break;
+  }
+}
+// Calculamos el total a pagar sumando los precios de los cursos en el carrito
+const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+console.log(`el total a pagar por su compra es: $${total} USD.`);
+alert(
+  "Le invito a revisar el total en la consola, SI agregó un producto le saldra resumen a pagar."
+);
